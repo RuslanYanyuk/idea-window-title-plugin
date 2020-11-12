e5a2503c1aa020913b2b73a305d2bf083c8fc0c1
Ruslan Yaniuk <ruslan.yanyuk@gmail.com>
2020-11-12 11:52:47 +0200

    Add base version

---
+++ src/ry/idea/plugin/CustomFrameTitleBuilder.java
@@ -0,0 +1,53 @@
+package ry.idea.plugin;
+
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.impl.FrameTitleBuilder;
+import com.intellij.openapi.wm.impl.PlatformFrameTitleBuilder;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author Ruslan Yaniuk
+ * @date Nov 2020
+ */
+@State(
+        name = "ry.idea.plugin.CustomFrameTitleBuilder",
+        storages = {@Storage("WindowTitlePlugin.xml")}
+)
+public class CustomFrameTitleBuilder extends FrameTitleBuilder
+        implements PersistentStateComponent<CustomFrameTitleBuilder> {
+    private static final FrameTitleBuilder DEFAULT_BUILDER = new PlatformFrameTitleBuilder();
+
+    public String titlePrefix = "IntelliJ IDEA - ";
+
+    public static CustomFrameTitleBuilder getInstance() {
+        return (CustomFrameTitleBuilder) ServiceManager.getService(FrameTitleBuilder.class);
+    }
+
+    @Override
+    public String getProjectTitle(@NotNull Project project) {
+        return titlePrefix + DEFAULT_BUILDER.getProjectTitle(project);
+    }
+
+    @Override
+    public String getFileTitle(@NotNull Project project, @NotNull VirtualFile virtualFile) {
+        return virtualFile.getPath();
+    }
+
+    @Nullable
+    @Override
+    public CustomFrameTitleBuilder getState() {
+        return this;
+    }
+
+    @Override
+    public void loadState(@NotNull CustomFrameTitleBuilder customFrameTitleBuilder) {
+        XmlSerializerUtil.copyBean(customFrameTitleBuilder, this);
+    }
+}
---
+++ src/ry/idea/plugin/settings/TitleSettings.java
@@ -0,0 +1,66 @@
+package ry.idea.plugin.settings;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.util.NlsContexts;
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBTextField;
+import com.intellij.util.ui.FormBuilder;
+import org.jetbrains.annotations.Nullable;
+import ry.idea.plugin.CustomFrameTitleBuilder;
+
+import javax.swing.*;
+
+/**
+ * @author Ruslan Yaniuk
+ * @date Nov 2020
+ */
+public class TitleSettings implements Configurable {
+    private final JPanel panel;
+    private final JBTextField titlePrefix = new JBTextField();
+
+    public TitleSettings() {
+        JLabel hint = new JLabel("Changes will take an effect after the restart");
+
+        hint.setEnabled(false);
+        panel = FormBuilder.createFormBuilder()
+                .addLabeledComponent(new JBLabel("Title prefix: "), titlePrefix, 1, false)
+                .addComponent(hint, 5)
+                .addComponentFillVertically(new JPanel(), 0)
+                .getPanel();
+    }
+
+    @Override
+    public @NlsContexts.ConfigurableName String getDisplayName() {
+        return "Window Title";
+    }
+
+    @Nullable
+    @Override
+    public JComponent createComponent() {
+        return panel;
+    }
+
+    @Override
+    public boolean isModified() {
+        CustomFrameTitleBuilder service = CustomFrameTitleBuilder.getInstance();
+
+        return !titlePrefix.getText().equals(service.titlePrefix);
+    }
+
+    @Override
+    public void apply() {
+        String text = titlePrefix.getText() == null ? "" : titlePrefix.getText();
+        CustomFrameTitleBuilder service = CustomFrameTitleBuilder.getInstance();
+
+        service.titlePrefix = text;
+        ApplicationManager.getApplication().restart();
+    }
+
+    @Override
+    public void reset() {
+        CustomFrameTitleBuilder service = CustomFrameTitleBuilder.getInstance();
+
+        titlePrefix.setText(service.titlePrefix);
+    }
+}
